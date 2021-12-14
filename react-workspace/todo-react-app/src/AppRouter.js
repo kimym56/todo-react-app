d83d17bdf2bb4fd23c5ea28d2b23b50f3c5a96bf
kimym56 <74525390+kimym56@users.noreply.github.com>
2021-12-15 00:12:47 +0900

    FE

---
+++ react-workspace/todo-react-app/src/AppRouter.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import "./index.css";
+import App from "./App";
+import Login from "./Login";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography"
+import SignUp from './SignUp';
+
+function Copyright(){
+    return(
+        <Typography variant="body2" color="textSecondary" align="center">
+            {"Copyright ⓒ"}
+            fsoftwareengineer, {new Date().getFullYear()}
+            {"."}
+        </Typography>
+    )
+}
+export default class AppRouter extends Component {
+    render() {
+        return (
+            <div>
+                <Router>
+                    <div>
+                        <Routes>
+                            <Route path="/login" element={<Login/>}/>
+                            <Route path="/" element={<App/>}/>
+                            <Route path="/signup" element={<SignUp/>}/>
+                        </Routes>
+                    </div>
+                    <Box mt={5}>
+                        <Copyright/>
+                    </Box>
+                </Router>
+            </div>
+        )
+    }
+}
